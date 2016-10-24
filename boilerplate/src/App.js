d289e48c08af647bc9d8fc89a9adf99435df3ac3
leecade <leecade@163.com>
2016-10-24 18:51:11 +0800

    ci

--- boilerplate/src/App.js
+++ boilerplate/src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       <div className='App'>
         <div className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to React {this.state.a}</h2>
+          <h2>{{ name }}: {{ description }} {this.state.a}</h2>
         </div>
         <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
